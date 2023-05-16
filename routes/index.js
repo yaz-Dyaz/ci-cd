15caabf71f7c04d43f2dbaead8f31e42bf7c731d
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2023-05-16 20:19:51 +0700

    add ci/cd

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ const user = require('../controllers/user');
 
 const middlewares = require('../utils/middlewares');
 
+router.get('/', (req, res) => {
+  return res.status(200).json({
+    status: true,
+    message: 'Welcome to auth api!',
+    data: null
+  });
+});
+
 router.post('/auth/register', user.register);
 router.post('/auth/login', user.login);
 router.get('/auth/whoami', middlewares.auth, user.whoami);
